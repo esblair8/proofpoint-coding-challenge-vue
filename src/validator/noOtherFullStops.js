62a25ae4a895ed141077ab686dc86a136adf8a1d
Evan Blair <esblair8@gmail.com>
2018-11-12 23:24:14 +0000

    added js doc for other full stop validator

--- src/validator/noOtherFullStops.js
+++ src/validator/noOtherFullStops.js
@@ -1,3 +1,11 @@
+/**
+ * Function that tests if the there are any other full stops in the sentence
+ * by first removing the last character regarldess of which character it is. 
+ * This ensures the final full stop, if present, does not give a false positive result
+ *
+ * @param   sentence - the sentence to validate
+ * @return  boolean
+ */
 export default sentence => {
     return !sentence
         .slice(0, -1)          
